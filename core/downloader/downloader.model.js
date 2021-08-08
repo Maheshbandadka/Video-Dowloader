bb76b41b55241cd0f6128518a753af92cdc00ac5
DESKTOP-0LTI3GD\Mahesh <maheshglobussoft@gmail.com>
2021-08-09 00:08:45 +0530

    [x] Updated code

--- core/downloader/downloader.model.js
+++ core/downloader/downloader.model.js
@@ -27,10 +27,10 @@ class DownloaderLis {
       logger.info(`url_media  ${url_media}`)
 
       axios.get('https://www.instagram.com/p/CSP120tBuvL/?utm_source=ig_web_copy_link'
-        // proxy: {
-        //   host: process.env.host,
-        //   port: process.env.PORT
-        // }
+        , {
+          headers: {
+            'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/12.0 Mobile/15E148 Safari/604.1'
+          }}
       ).then(result => {
         logger.info(`element  ${result}`)
 
